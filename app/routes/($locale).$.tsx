56be53381d8c8a3fb86ff1d599333e447f430e36
rashmilkw62 <164094489+rashmilkw62@users.noreply.github.com>
2025-05-25 02:45:30 +0530

    Initial commit

---
+++ app/routes/($locale).$.tsx
@@ -0,0 +1,18 @@
+import type { LoaderFunctionArgs } from "@shopify/remix-oxygen";
+import { validateWeaverseData, WeaverseContent } from "~/weaverse";
+
+export async function loader({ context }: LoaderFunctionArgs) {
+  let weaverseData = await context.weaverse.loadPage({
+    type: "CUSTOM",
+  });
+
+  validateWeaverseData(weaverseData)
+
+  return {
+    weaverseData,
+  };
+}
+
+export default function Component() {
+  return <WeaverseContent />;
+}
